01c03ff5c37787d70c46a12c6f5fa94ca7f19390
Siarhei Liavontsyeu <leontevsi@gmail.com>
2019-10-04 17:35:17 +0300

    feat: implement solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,36 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    // create empty game object 
+    constructor() {
+        this.answer = 0;
+        this.min = 0;
+        this.max = 0;
     }
 
-    guess() {
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
 
+    //binary search algorithm
+    guess() {
+        if(this.max < this.min){
+            return -1;
+        }
+        this.answer = Math.round(this.max - (this.max - this.min)/2);
+        // console.log(this);
+        return this.answer;
+        
     }
 
     lower() {
-
+        this.max = this.answer;
     }
 
     greater() {
-
+        this.min = this.answer;
     }
+    
 }
 
-module.exports = GuessingGame;
+
+ module.exports = GuessingGame;
